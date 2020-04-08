80c5388dc4f5fa26bea51a1c47d5eca51e721bbc
Pranitha05032002 <62944229+Pranitha05032002@users.noreply.github.com>
2020-04-08 09:55:06 +0530

    Add files via upload

---
+++ Sorting.java
@@ -0,0 +1,28 @@
+mport java.util.Arrays;
+	import java.util.Comparator;
+	
+
+	public class Sorting
+	{
+	  public static void selectionSort(int[] arr)
+	  {
+	    for (int i = 0; i < arr.length - 1; ++i)
+	    {
+	      int minIndex = i;
+	      for (int j = i + 1; j < arr.length; ++j)
+	      {
+	        if (arr[j] < arr[minIndex])
+	        {
+	          minIndex = j;
+	        }
+	      }
+	      int temp = arr[i];
+	      arr[i] = arr[minIndex];
+	      arr[minIndex] = temp;
+	    }
+
+		}
+
+	  }
+
+
